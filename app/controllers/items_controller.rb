292fcaba43f3688d9acf062d652e5e168c2d3bb0
Nicolas Boulet <nicolasboulet@live.fr>
2020-01-18 13:12:19 +0100

    create item controller not working waiting master merge

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,10 +8,29 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
     @items = policy_scope(Item)
+    authorize @items
   end
 
   def new
     @item = Item.new
     authorize @item
   end
+
+  def create
+    @item = current_user.items.build(item_params)
+    if item.save
+      redirect_to items_path
+    else
+      render new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :category, :tag, :description,
+                                 :price_in_cents, :address, :availability,
+                                 :start_date, :end_date, :latitude, :longitude)
+  end
+
 end
