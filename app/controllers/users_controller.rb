153b36cdcc3ce2bea6df84bcbdce1378ef4305cb
quentinlallemand <quentinlallemand@gmail.com>
2020-01-23 00:44:53 +0100

    add cloudinary logic on current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,5 +21,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:first_name, :last_name, :photo, :email)
   end
-
+# comment gerer lorsque l'image est vide --> erreur
 end
